844d2890ecf4c90b6c5fa9b1e79c4052978786f5
Nico Lachmuth <nico@nlachmuth.de>
2021-03-16 20:07:07 +0100

    inital commit

---
+++ src/TinyMCE.BlazorWasmExample/MyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyMCE.Blazor.Sample.BlazorWasm
+{
+    public class MyModel
+    {
+        [Required]
+        public string Html { get; set; }
+    }
+}
